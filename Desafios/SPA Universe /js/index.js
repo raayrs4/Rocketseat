bb39bc60c8299d080ea57e2750b6f70e0e90e7e5
Rayane Santos <rayanesantos@Rayanes-MacBook-Pro.local>
2023-01-12 18:02:54 -0300

    projeto como alguns bugs

--- Desafios/SPA Universe /js/index.js
+++ Desafios/SPA Universe /js/index.js
@@ -1,13 +1,14 @@
 import {RouterClass} from "./router.js"
 
 // mapeamento da rota para achar a página que eu quero
-const router = new RouterClass()
-router.registerPage('/', "/pages/home.html")
-router.registerPage('/ouniverso', "/pages/ouniverso.html")
+const router = new RouterClass()  // aqui eu salvo a class "RouterClass" na variável router para poder usar posteriormente
+
+router.registerPage('/', "/pages/home.html")  // usando o método "registerPage" para registrar as routers -- o método é como chamamos a função que está dentro de uma classe, como ocorre com "registerPage"
+router.registerPage('/ouniverso', "/pages/ouniverso.html") // salvando nome e link (routeName, link) como especificado no projeto/forma "router.js"
 router.registerPage('/exploracao', "/pages/exploracao.html")
 router.registerPage(404, "/pages/404.html")
 
-await router.getHTMtofNewPage() //chamo a função handle logo de inicio pra ler o html referente a page "home"
+await router.getHTMLtoNewPage() //chamo a função "getHTMtofNewPage" logo de inicio pra ler o html referente a page "home" para carregar o html dela, o await serve para que a execução do códido espere por essa parte para poder continuar 
 
 const buttonHome = document.querySelector(".initiation")
 const buttonExploracao = document.querySelector(".exploration")
@@ -16,9 +17,9 @@ const body = document.querySelector("body")
 const app = document.querySelector("#app")
 const buttonQueroSaberMais = document.querySelector(".moreInfo")
 
-
-window.onpopstate = () => router.getHTMtofNewPage() // para poder mudar as páginas pela setinha do navegador 
-window.route = () => router.getHTMtofNewPage() // uma função que vai disparar a função route, pq não estava funcionando no navegador, ou posso colocar também o window.route() lá no index.html
+window.onpopstate = () => router.getHTMLtoNewPage() // para poder mudar as páginas pela setinha do navegador 
+// window.route = () => router.getHTMLtoNewPage() // uma função que vai disparar a função route, pq não estava funcionando no navegador, ou posso colocar também o window.route() lá no index.html
+window.addNewPageToHistory = () => router.addNewPageToHistory() // uma função que vai disparar a função route, pq não estava funcionando no navegador, ou posso colocar também o window.route() lá no index.html
 
 buttonHome.addEventListener('click', function () {
   removeAllBackgrounds()
@@ -37,22 +38,13 @@ buttonExploracao.addEventListener('click', function () {
 })
 
 buttonOUniverso.addEventListener('click', function () {
-  removeAllBackgrounds()
-  removeHighlight()
-  body.classList.add("ouniverso")
-  buttonOUniverso.classList.add("active")
-  removeHomeConfigurations()
+    removeAllBackgrounds()
+    removeHighlight()
+    body.classList.add("ouniverso")
+    buttonOUniverso.classList.add("active")
+    removeHomeConfigurations()
 })
 
-buttonQueroSaberMais.addEventListener('click', function(){
-  router.addNewPageToHistoryString('ouniverso')
-  removeAllBackgrounds()
-  removeHighlight()
-  body.classList.add("ouniverso")
-  buttonOUniverso.classList.add("active")
-  removeHomeConfigurations()
-  
-})
 
 function removeAllBackgrounds() {
   body.classList.remove("ouniverso");
@@ -69,3 +61,12 @@ function removeHighlight() {
 function removeHomeConfigurations() {
   app.classList.remove("homeConfigurations")
 }
+
+buttonQueroSaberMais.addEventListener('click', function(){
+  router.addNewPageToHistoryString('ouniverso')
+  removeAllBackgrounds()
+  removeHighlight()
+  body.classList.add("ouniverso")
+  buttonOUniverso.classList.add("active")
+  removeHomeConfigurations()
+})
--- Desafios/SPA Universe /js/router.js
+++ Desafios/SPA Universe /js/router.js
@@ -1,51 +1,48 @@
 export class RouterClass {
 
-  routes = {}
+  routes = {} // declarando o objeto
 
-  registerPage(routeName, link) {
-    this.routes[routeName] = link
+  registerPage(routeName, link) {   // função dentro da classe "RouterClass" que é chamada de método, ela tá definindo com as pages devem ser registradas. Recebendo (routeName, link)
+    this.routes[routeName] = link  // fazendo o que aqui?
   }
 
-  // a funcão route vai tirar o padrão de recarrgamento da página quando eu clicar em algum outro link
-  addNewPageToHistory(event) {  
-    event = event || window.event // verificando se o evento foi passado, geralzão mesmo pq é para todos os eventos 
+  // a funcão "addNewPageToHistory" vai tirar o padrão de recarregamento da página quando eu clicar em algum outro link e colocar o pathname que mudou no topo do his†órico
+  addNewPageToHistory(event) {
+    event = event || window.event // verificando se o evento foi passado, geralzão mesmo pq é para todos os eventos que acontecerem na window
     event.preventDefault() // evitar o padrão de recarregamento
   
-    window.history.pushState({}, "", event.target.href) //para fazer o pathname mudar -- "pushState" para colocar o estado no histórico, pegar o href de quem disparou o evento e colocar no histórico
+    window.history.pushState({}, "", event.target.href) //para fazer o pathname mudar -- usamos o "pushState" para colocar o estado no histórico, pegar o href de quem disparou o evento e colocar no histórico
   
-    this.getHTMtofNewPage() //dentro da instância, sempre que eu quiser usar uma função ou uma propriedade, eu preciso usar o this, pois ele vai ser referência a alguma coisa aqui dentro do par de chaves
+    this.getHTMLtoNewPage() //dentro da instância, sempre que eu quiser usar uma função ou uma propriedade, eu preciso usar o this, pois ele vai ser referência a alguma coisa aqui dentro do par de chaves
   }
 
   addNewPageToHistoryString(rota) {
     window.history.pushState({}, "", rota) //para fazer o pathname mudar -- "pushState" para colocar o estado no histórico, pegar o href de quem disparou o evento e colocar no histórico
   
-    this.getHTMtofNewPage()
+    this.getHTMLtoNewPage() // Chamo essa função para pegar o HTML da página que ficou por último no meu histórico, portanto a última que eu cliquei 
   }
 
-  async getHTMtofNewPage() {
-    const {pathname} = window.location //desestruturação do objeto para ficar mais fácil de pegar as propriedades do objeto
-    const route = this.routes[pathname] || this.routes[404] // mudar o conteudo do link pegando alguma rota de página ou então mostrar a página de erro 404
-   
-    console.info({route})
+  async getHTMLtoNewPage() {  // assíncrono por que eu quero esperar pelo fetch
+    const {pathname} = window.location //desestruturação do objeto para ficar mais fácil de pegar as propriedades do objeto -- a página que vai estar no window location é a última do meu histórico e vou pegar o pathname dela 
+    const route = this.routes[pathname] || this.routes[404] // mudar o conteudo do link pegando alguma rota de página ou então mostrar a página de erro 404, preciso usar o "routes[pathname]" por que é uma das propriedades do objeto "routes" e salvar na variável "route"
+ 
     // pegando o conteudo de uma outra página html e jogando numa página com uma "promessa" pra ficar assíncrono
-    await fetch(route)
-    .then(this.getTextFromData)
-    .then(this.assignHTMLToApp)
+    await fetch(route)  // passando como  argumento "route", que é a rota da última página que eu visitei 
+    .then(this.getTextFromData) // pegar os textos da propriedade data (que tem o HTML)
+    .then(this.assignHTMLToApp) // colocar o HTML dentro da div #app
+  }
+
+    // private
+    // data => data.text()
   
-    console.log(pathname)
+  getTextFromData (data) {  // pegar os textos da propriedade data (que tem o HTML) e retornar 
+      return data.text() // por que retorno data.text e na função de baixo uso HTML como argumento?
   }
 
    // private
-  //data => data.text()
-  getTextFromData (data) {
-      return data.text()
-  }
-
-   // private
-   assignHTMLToApp (html) {
-    const appElement = document.querySelector("#app")
+  assignHTMLToApp (html) { // colocando o HTML da página dentro da div #app para mudar só ela, e não a página toda
+    const appElement = document.querySelector("#app") // salvando a variável para ser usada depois 
     appElement.innerHTML = html // innerHTML - O html interno do elemento app será o html recebido 
   }
-
  
 }
