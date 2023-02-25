57c614bb6c9090bae167914c680974401135eda0
Rayane <raay.rs4@gmail.com>
2023-02-25 18:52:56 -0300

    criação do banco de dados

---
+++ Desafios/CadastroFilme/src/routes/users.routes.js
@@ -0,0 +1,11 @@
+const {Router} = require("express")          // Router é uma funcionalidade dentro da biblioteca express
+
+const UsersController = require("../controllers/UsersController")
+
+const usersController = new UsersController()
+
+const usersRoutes = Router()                 // inicializando o Router
+
+usersRoutes.post("/", usersController.create)
+
+module.exports = usersRoutes
