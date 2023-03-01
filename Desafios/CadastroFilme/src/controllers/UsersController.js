c977ed508c572e688dc6dd8a65070602e73b31ba
Rayane <raay.rs4@gmail.com>
2023-02-28 21:51:36 -0300

    criação do NotesController

--- Desafios/CadastroFilme/src/controllers/UsersController.js
+++ Desafios/CadastroFilme/src/controllers/UsersController.js
@@ -1,14 +1,85 @@
+const {hash, compare} = require("bcryptjs")
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite");
+const createUsers = require("../database/sqlite/migrations/createUsers");
+
 
 class UsersController {
-  create (request, response) {
+  async create (request, response) {
     const {name, email, password} = request.body;
 
-    if(!name) {
-      throw new AppError("O nome é obrigatório");
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    console.log({checkUserExists})
+
+    if(checkUserExists) {
+      console.log("existe")
+      throw new AppError("Este e-mail já está em uso")
     }
 
-    response.status(201).json({name, email, password})
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    )
+
+    return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const {name, email, password, old_password} = request.body
+    const {id} = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado")
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso")
+    }
+
+    // passou por tudo, então posso mudar:
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if(password && !old_password){
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    try {
+      await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        password = ?,
+        updated_at = DATETIME('now')
+        WHERE id = ?`,
+        [user.name, user.email, user.password, id]
+      )
+    } catch (error) {
+      console.log({error})
+      throw error
+    }
+    
+    return response.json()
   }
 }
 
--- Desafios/CadastroFilme/src/server.js
+++ Desafios/CadastroFilme/src/server.js
@@ -1,24 +1,24 @@
 require("express-async-errors")  // importação do express-asyn-errors
 
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 
 const express = require("express")
 
 const routes = require("./routes")
 
+migrationsRun()
+
 const app = express()
 app.use(express.json())
 
 app.use(routes)
 
-database()
-
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {  
     return response.status(error.statusCode).json({
       status: "error",
-      message: "error.message"
+      message: error.message
     })
   }
 
